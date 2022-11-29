e0a7c0dd68dee6c7700c327aa9fa2f9e0b82b000
asters1 <gj744559140@163.com>
2022-11-29 06:38:25 +0000

    更新

--- legado/updateLegado.sh
+++ legado/updateLegado.sh
@@ -7,7 +7,11 @@ asters1Url="https://github.com/asters1/legado"
 echo $asters1Url
 
 legadoCommit=$(curl -sL ${legadoUrl}/commits/master |grep -o "/gedoor/legado/commit/[a-z0-9]\+" |head -1 | cut -d\/ -f5)
+echo "legadoCommit:"
 echo "${legadoCommit}"
+asters1Commit=$(curl -sL ${asters1Url}/commits/master |grep -o "/asters1/legado/commit/[a-z0-9]\+" |head -1 | cut -d\/ -f5)
+echo "asters1Commit:"
+echo "${asters1Commit}"
 
 
 
