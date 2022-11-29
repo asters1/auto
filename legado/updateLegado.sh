b1a7bb487d3b6c81e38c362c607d11432e9f77c9
asters1 <gj744559140@163.com>
2022-11-29 06:15:43 +0000

    更新

--- legado/updateLegado.sh
+++ legado/updateLegado.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo "仓库地址:"
-echo "https://github.com/gedoor/legado"
 legadoUrl="https://github.com/gedoor/legado"
 echo $legadoUrl
+echo "我的仓库地址:"
+asters1Url="https://github.com/asters1/legado"
+echo $asters1Url
+
 
