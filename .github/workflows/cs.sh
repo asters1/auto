619746f829783a1e322660f5010614edea615b3f
asters1 <gj744559140@163.com>
2022-12-09 00:35:47 +0000

    测试yml

--- .github/workflows/cs.sh
+++ .github/workflows/cs.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-Change_file_path=app/src/main/java/com/github/catvod/spider/
-if [ ${Change_file_path:0:42} == $Change_file_path ];then
-    echo "匹配成功"
-else
-    echo "失败"
-fi
+a=app/src/main/java/com/github/catvod/spider/TX.java
+echo "${a##*/}"
